38cb3f78796a7f7e68082436b2e3a5a5088b40e7
prushton2 <peterrushton418@gmail.com>
2023-09-18 09:47:51 -0400

    added canvas arg to users code

--- script.py
+++ script.py
@@ -1,7 +1,9 @@
 from picobot import move, state, stateFunctions
 # This picobot relies on states to run, starting at state 0
 # To define a state, create a function with a decorator of @state(<state id>)
+# Include an argument for the canvas inside your state function, and include this as a parameter when calling
+# a move or check function
 # ex:
 # @state(0)
-# def state0fn():
-#   move(N)
+# def state0fn(c):
+#   move(c, N)
